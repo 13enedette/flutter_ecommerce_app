0c5beb0a6dbada1c4c5d83f0797c130b3a78a283
13enedette <164311718+13enedette@users.noreply.github.com>
2026-04-09 11:05:14 +0300

    Create homescreen.dart

---
+++ homescreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to your Clothing Store 👗',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
